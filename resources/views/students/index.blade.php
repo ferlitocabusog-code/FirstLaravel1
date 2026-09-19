fe602726755176666cb3cc8f6a20208f039e4e61
ferlitocabusog-code <ferlitocabusog@gmail.com>
2026-09-19 13:59:18 +0000

    Complete Laravel project

---
+++ resources/views/students/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Students Ni Opaw</title>
+</head>
+<body>
+
+    <h1>Student Page Ni Opaw</h1>
+
+    <p>Welcome to the Student Page Ni Opaw. SUP Y'LL NIGGAS.</p>
+
+</body>
+</html>
---
+++ resources/views/subjects/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Subjects Ni Opaw</title>
+</head>
+<body>
+
+    <h1>Subject Page Ni Opaw</h1>
+
+    <p>Welcome to the Subject Page Ni Opaw. SUP Y'LL NIGGAS.</p>
+
+</body>
+</html>
